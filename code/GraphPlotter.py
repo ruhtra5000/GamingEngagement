7e33b022557301d59d369b50cd11e6a1f2e3f094
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-07 03:12:37 -0300

    refactor - code commenting and changing some line's position

--- code/GraphPlotter.py
+++ code/GraphPlotter.py
@@ -2,15 +2,25 @@ import pandas
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+# Module for plotting graphs (mainly likelihood graphs)
+
 # Graph for continuous feature
 def plotLikelihood(dataset, feature, target="EngagementLevel"):
     plt.figure(figsize=(8, 5))
 
     for cls in dataset[target].unique():
         subset = dataset[dataset[target] == cls]
-        sb.kdeplot(subset[feature], fill=True, alpha=0.4, label=f"{target}={cls}")
+        sb.kdeplot(
+            subset[feature], 
+            fill=True, 
+            alpha=0.35, 
+            label=f"{target}={cls}"
+        )
     
     plt.title(f"Distribuição de {feature} para {target}")
+    plt.xlabel(feature)
+    plt.ylabel(f"P({target} | {feature})")
+   
     plt.show()
 
 #Graph for categorical feature
@@ -25,8 +35,8 @@ def plotLikelihoodCategorical(dataset, feature, target="EngagementLevel"):
 
     probTable.plot(kind="bar", alpha=0.7)
     plt.title(f"Distribuição de {feature} para {target}")
-    plt.ylabel("P(x | y)")
     plt.xlabel(feature)
+    plt.ylabel(f"P({target} | {feature})")
     plt.legend(title=target)
-    
+
     plt.show()
