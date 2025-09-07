ea8cd965588df3fcf4916eb8786f710af59af92e
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-07 04:36:24 -0300

    feat - importing Naive Bayes classifier

--- code/Engagement.py
+++ code/Engagement.py
@@ -1,9 +1,12 @@
 import pandas
 from PreProcessing import preProcessing
 from GraphPlotter import plotLikelihood, plotLikelihoodCategorical
+from NaiveBayes import training
 
 dataset = pandas.read_csv('online_gaming_behavior_dataset.csv')
-#dataset = preProcessing(dataset)
+dataset = preProcessing(dataset)
+
+training(dataset)
 
 #plotLikelihood(dataset, "AchievementsUnlocked")
-plotLikelihoodCategorical(dataset, "GameDifficulty")
+#plotLikelihoodCategorical(dataset, "InGamePurchases")
