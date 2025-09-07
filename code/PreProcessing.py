39bbd68aa654123e1b47afc80eeab997c2072853
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-07 04:29:28 -0300

    feat - removing some features and adding frequency encoding

--- code/PreProcessing.py
+++ code/PreProcessing.py
@@ -2,22 +2,20 @@ import pandas
 
 # Module for data preprocessing 
 
-def preProcessing(dataset):
-    # Removing PlayerID column
-    dataset = dataset.drop(columns=["PlayerID"])
+def preProcessing(dataset: pandas.DataFrame):
+    # Removing PlayerID, Gender and InGamePurchases columns
+    dataset = dataset.drop(columns=["PlayerID", "Gender", "InGamePurchases"])
 
-    # Transforming Gender, GameDifficulty, EngagementLevel into integer value
-    dataset = dataset.rename(columns={"Gender": "Gender_Male"})
-    dataset["Gender_Male"] = dataset["Gender_Male"].replace({"Male": 1, "Female": 0})
-
+    # Transforming GameDifficulty, EngagementLevel into integer value ()
     dataset["GameDifficulty"] = dataset["GameDifficulty"].replace({"Easy": 0, "Medium": 1, "Hard": 2})
 
     dataset["EngagementLevel"] = dataset["EngagementLevel"].replace({"Low": 0, "Medium": 1, "High": 2})
         
-    print(dataset.describe())
-    print(dataset.dtypes) 
+    # Adding Location and GameGenre relative frequency (Freq. Encoding)
+    locationFreq = dataset["Location"].value_counts() / len(dataset)
+    gameGenreFreq = dataset["GameGenre"].value_counts() / len(dataset)
 
-    print(f"\nLocation values: {dataset["Location"].unique()}")
-    print(f"\nGame genre values: {dataset["GameGenre"].unique()}")
+    dataset["LocationFreq"] = dataset["Location"].map(locationFreq)
+    dataset["GameGenreFreq"] = dataset["GameGenre"].map(gameGenreFreq)
 
     return dataset
