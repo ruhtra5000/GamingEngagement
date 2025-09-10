046fded857b3d8dc61b342f261e380be94ee5966
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-10 05:11:08 -0300

    fix - removing freq. encoding for GameGenre and Location

--- code/PreProcessing.py
+++ code/PreProcessing.py
@@ -6,16 +6,9 @@ def preProcessing(dataset: pandas.DataFrame):
     # Removing PlayerID, Gender and InGamePurchases columns
     dataset = dataset.drop(columns=["PlayerID", "Gender", "InGamePurchases"])
 
-    # Transforming GameDifficulty, EngagementLevel into integer value ()
+    # Transforming GameDifficulty and EngagementLevel into integer value (Label Encoding)
     dataset["GameDifficulty"] = dataset["GameDifficulty"].replace({"Easy": 0, "Medium": 1, "Hard": 2})
-
+    
     dataset["EngagementLevel"] = dataset["EngagementLevel"].replace({"Low": 0, "Medium": 1, "High": 2})
-        
-    # Adding Location and GameGenre relative frequency (Freq. Encoding)
-    locationFreq = dataset["Location"].value_counts() / len(dataset)
-    gameGenreFreq = dataset["GameGenre"].value_counts() / len(dataset)
-
-    dataset["LocationFreq"] = dataset["Location"].map(locationFreq)
-    dataset["GameGenreFreq"] = dataset["GameGenre"].map(gameGenreFreq)
 
     return dataset
