6801a1e3ae7144cdfbee25df986f9f6e7166244f
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-11 02:03:40 -0300

    fix - removing other columns

--- code/PreProcessing.py
+++ code/PreProcessing.py
@@ -3,12 +3,17 @@ import pandas
 # Module for data preprocessing 
 
 def preProcessing(dataset: pandas.DataFrame):
-    # Removing PlayerID, Gender and InGamePurchases columns
-    dataset = dataset.drop(columns=["PlayerID", "Gender", "InGamePurchases"])
+    # Removing columns
+    dataset = dataset.drop(columns=[
+        "PlayerID", 
+        "Gender", 
+        "InGamePurchases", 
+        "GameGenre", 
+        "Location",
+        "GameDifficulty"
+    ])
 
-    # Transforming GameDifficulty and EngagementLevel into integer value (Label Encoding)
-    dataset["GameDifficulty"] = dataset["GameDifficulty"].replace({"Easy": 0, "Medium": 1, "Hard": 2})
-    
+    # Transforming EngagementLevel into integer value (Label Encoding)
     dataset["EngagementLevel"] = dataset["EngagementLevel"].replace({"Low": 0, "Medium": 1, "High": 2})
 
     return dataset
